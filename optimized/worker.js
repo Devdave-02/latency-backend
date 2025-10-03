01cd2fcba96ac6eddc9e154e4be3527933b3c75f
Devdave-02 <davidokanla69@gmail.com>
2025-10-03 19:43:17 +0100

    Update worker.js

--- optimized/worker.js
+++ optimized/worker.js
@@ -1,12 +1,11 @@
 const { parentPort, workerData } = require("worker_threads");
 
-// CPU-heavy task: recursive Fibonacci
 function heavyTask(n) {
   if (n <= 1) return 1;
   return heavyTask(n - 1) + heavyTask(n - 2);
 }
 
-// Run the task and send result back
 const result = heavyTask(workerData.number);
 parentPort.postMessage(result);
+
 
